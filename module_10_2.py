33ccd9d71c620c8cc22d353d0fc2c5b306f7c71c
Romashka-Al <alinashkunova@yandex.ru>
2024-09-29 23:14:05 +0400

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -1,11 +1,31 @@
 from threading import Thread
-import requests
+from time import sleep
 
 
 class Knight(Thread):
     def __init__(self, name, power):
+        Thread.__init__(self)
         self.name = name
         self.power = power
 
     def run(self):
-        ee
+        print(f'{self.name}, на нас напали!')
+        x = 100
+        time = 0
+        while x > 0:
+            x -= self.power
+            time += 1
+            print(f'{self.name}, сражается {time} дней, осталось {x} воинов')
+            sleep(1)
+        print(f'{self.name} одержал победу спустя {time} дней!')
+
+
+knight1 = Knight('Sir Lancelot', 10)
+knight2 = Knight("Sir Galahad", 20)
+
+knight1.start()
+knight2.start()
+
+knight1.join()
+knight2.join()
+print('Все битвы закончились!')
