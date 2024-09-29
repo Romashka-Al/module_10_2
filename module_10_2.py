77a388e7dd259dbbb46846759862d3abcb406825
Romashka-Al <alinashkunova@yandex.ru>
2024-09-29 21:20:01 +0400

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,11 @@
+from threading import Thread
+import requests
+
+
+class Knight(Thread):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def run(self):
+        ee
